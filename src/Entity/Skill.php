e128e49b1108a40983c6e700e00732b1fc805daa
Sudre-Julien01 <103562035+Sudre-Julien01@users.noreply.github.com>
2022-06-09 17:30:19 +0200

    enlever niveaux skills

--- src/Entity/Skill.php
+++ src/Entity/Skill.php
@@ -29,18 +29,7 @@ class Skill
      */
     private ?string $name = null;
 
-    /**
-     * @var int|null
-     * @ORM\Column(type="integer")
-     * @Assert\NotBlank(message="Ce champs ne peut pas être vide.")
-     * @Assert\Range(
-     *     min=1,
-     *     max=10,
-     *     minMessage="Le niveau doit être supérieur ou égal à 1",
-     *     maxMessage="Le niveau doit être inférieur ou égal à 10"
-     * )
-     */
-    private ?int $level = null;
+
 
     /**
      * @return int|null
@@ -66,19 +55,5 @@ class Skill
         $this->name = $name;
     }
 
-    /**
-     * @return int|null
-     */
-    public function getLevel(): ?int
-    {
-        return $this->level;
-    }
 
-    /**
-     * @param int|null $level
-     */
-    public function setLevel(?int $level): void
-    {
-        $this->level = $level;
-    }
 }
