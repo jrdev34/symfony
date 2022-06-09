4e4925bf3a87e10c303ffa36abe2e70e069c5cf7
Sudre-Julien01 <103562035+Sudre-Julien01@users.noreply.github.com>
2022-06-09 17:41:44 +0200

    retour en arriere

--- src/Entity/Skill.php
+++ src/Entity/Skill.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * Class Skill
  * @package App\Entity
  * @ORM\Entity(repositoryClass="App\Repository\SkillRepository")
- *  @ApiResource()
+ * @ApiResource()
  */
 class Skill
 {
@@ -29,7 +29,18 @@ class Skill
      */
     private ?string $name = null;
 
-
+    /**
+     * @var int|null
+     * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="Ce champs ne peut pas être vide.")
+     * @Assert\Range(
+     *     min=1,
+     *     max=10,
+     *     minMessage="Le niveau doit être supérieur ou égal à 1",
+     *     maxMessage="Le niveau doit être inférieur ou égal à 10"
+     * )
+     */
+    private ?int $level = null;
 
     /**
      * @return int|null
@@ -55,5 +66,19 @@ class Skill
         $this->name = $name;
     }
 
+    /**
+     * @return int|null
+     */
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
 
+    /**
+     * @param int|null $level
+     */
+    public function setLevel(?int $level): void
+    {
+        $this->level = $level;
+    }
 }
